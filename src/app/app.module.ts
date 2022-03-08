de3e45f716fd3b67086075909381199e15feeaaa
Michael Isom <airizom@hotmail.com>
2022-03-08 07:30:31 -0600

    Add tabs and toolbar for showing search examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxListSearchModule } from 'projects/ngx-list-search/src/lib/ngx-list-search.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +18,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     NgxListSearchModule,
     BrowserAnimationsModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
