2fe1252bc51af5a60440f489014320eea8aa0a62
Michael Isom <airizom@hotmail.com>
2022-03-08 15:20:19 -0600

    Create ci pipeline

--- projects/ngx-list-search/src/public-api.ts
+++ projects/ngx-list-search/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-list-search
  */
 
-export * from './lib/ngx-list-search.service';
 export * from './lib/ngx-list-search.component';
 export * from './lib/ngx-list-search.module';
+
