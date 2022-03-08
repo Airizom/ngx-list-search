7ce19ed14dfd593b82b48d7ac800b63c222bcff3
Michael Isom <airizom@hotmail.com>
2022-03-08 12:15:49 -0600

    Create basic testbed with spectator

--- projects/ngx-list-search/src/lib/ngx-list-search.component.spec.ts
+++ projects/ngx-list-search/src/lib/ngx-list-search.component.spec.ts
@@ -1,25 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
+import { State } from 'country-state-city';
 import { NgxListSearchComponent } from './ngx-list-search.component';
+import { NgxListSearchModule } from './ngx-list-search.module';
 
 describe('NgxListSearchComponent', () => {
-  let component: NgxListSearchComponent;
-  let fixture: ComponentFixture<NgxListSearchComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NgxListSearchComponent ]
-    })
-    .compileComponents();
+  let spectator: SpectatorHost<NgxListSearchComponent>;
+  // Create host component with NgxListSearchComponent and MatListModule
+  const createHost = createHostFactory({
+    component: NgxListSearchComponent,
+    imports: [
+      NgxListSearchModule,
+      MatListModule,
+      ReactiveFormsModule
+    ]
   });
 
+  const states = State.getStatesOfCountry('US');
   beforeEach(() => {
-    fixture = TestBed.createComponent(NgxListSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createHost(`
+      <mat-list>
+        <ngx-list-search [placeholder]="'Search states...'"></ngx-list-search>
+        <mat-list-item *ngFor="let item of states">
+          {{ item.name }}
+        </mat-list-item>
+      </mat-list>
+    `, {
+      hostProps: {
+        states
+      }
+    });
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 });
